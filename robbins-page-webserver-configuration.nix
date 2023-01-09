54a0d6976a71946dc70f41a4c56e516113b8be4e
Nathanael Robbins <nejrobbins@gmail.com>
2023-01-09 16:47:00 -0500

    Setup Nginx

--- robbins-page-webserver-configuration.nix
+++ robbins-page-webserver-configuration.nix
@@ -1,5 +1,6 @@
 { config, lib, pkgs, inputs, ... }: {
   imports = [ "${inputs.nixpkgs-unstable}/nixos/modules/virtualisation/google-compute-image.nix" ];
+
   nixpkgs.hostPlatform = "x86_64-linux";
   system.stateVersion = "23.05";
   nix = {
@@ -14,4 +15,20 @@
     optimise.automatic = true;
     gc.automatic = true;
   };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."robbins.page" = {
+      root = "${inputs.website-src.packages.x86_64-linux.default}";
+      forceSSL = true;
+      sslCertificate = "/var/keys/robbins.page.pem";
+      sslCertificateKey = "/var/keys/robbins.page.key";
+      extraConfig = ''
+        ssl_client_certificate /etc/ssh/authenticated_origin_pull_ca.pem;
+        ssl_verify_client on;
+      '';
+    };
+  };
+  environment.systemPackages = [ inputs.website-src.packages.x86_64-linux.default ];
+  environment.etc."ssh/authenticated_origin_pull_ca.pem".text = builtins.readFile ./authenticated_origin_pull_ca.pem;
 }

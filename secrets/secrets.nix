a350e429a0fb5238bdff22f4008e6d85d9145c07
Nathanael Robbins <nejrobbins@gmail.com>
2025-07-17 21:05:01 -0400

    hopefully

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,10 @@
 let
-  nejrobbins_gmail_com = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICKBakFT1Zg1bsxXhAEKKLOVRxmFf/JwQ2Vym1N3tstk nejrobbins_gmail_com";
+  nejrobbins_gmail_com = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHBROs/XTz22QVUf1bhruq/FgrE+GHKkS77sR7Q3GJL2 nejrobbins_gmail_com";
   users = [ nejrobbins_gmail_com ];
 
-  robbins-page-webserver = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOow30s6wr72VcQe/Y5XaVBn1c4Zr9qXHv91z70zDsST";
+  robbins-page-webserver = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPj2sgu4rsEf2fBUcTII2bOmfA/U/XJ7k+g7Xba8ffW/";
   systems = [ robbins-page-webserver ];
 in
 {
-  #"cloudflare-api-token.age".publicKeys = users ++ systems;
+  "cloudflare-api-token.age".publicKeys = users ++ systems;
 }

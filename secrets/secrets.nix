a1b5183fec8df0f00e6601e4cb9b927844db8b0d
Nathanael Robbins <nejrobbins@gmail.com>
2025-07-18 21:07:32 -0400

    New host key again

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   nejrobbins_gmail_com = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHBROs/XTz22QVUf1bhruq/FgrE+GHKkS77sR7Q3GJL2 nejrobbins_gmail_com";
   users = [ nejrobbins_gmail_com ];
 
-  robbins-page-webserver = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPrqugYc4tXSN9lwrbLR4VY7enh87UiqSKaKGp1AQE8t";
+  robbins-page-webserver = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILnF/O8TKXr/EYoMHAu+hYA+zJ2kdCsgpoj3X1CsfGwg";
   systems = [ robbins-page-webserver ];
 in
 {
